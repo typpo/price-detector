f9a8bce6b27e2fd27fee4353327bdb79bb9c3e97
Ian <typppo@gmail.com>
2014-08-31 18:50:38 -0700

    popup and background working

--- chrome/main.js
+++ chrome/main.js
@@ -6,8 +6,7 @@ chrome.runtime.onInstalled.addListener(
   });
 
 function onClickHandler(info, tab) {
-  chrome.tabs.sendRequest(tab.id, "getLastClickedDomPath",
-    function(response) {
+  chrome.tabs.sendRequest(tab.id, "getLastClickedDomPath", function(response) {
       console.log(response);
       var xhr = new XMLHttpRequest();
       xhr.onreadystatechange = function() {
@@ -17,6 +16,15 @@ function onClickHandler(info, tab) {
             console.log("Saved");
             isSuccess = true;
             // TODO: alert user action is complete.
+            chrome.extension.sendMessage({
+              popup: true,
+              action: 'save',
+              url: tab.url,
+              success:true
+            },
+              function(response) {
+              console.log(response);
+            });
           } else {
             console.log("Error" + xhr.statusText);
           }
@@ -25,10 +33,10 @@ function onClickHandler(info, tab) {
         //chrome.extension.sendMessage(
         //  {action: 'save', success: isSuccess, url: tab.url},
         //  function(){});
-      };
+      };  // xhr.onreadystatechange
       var params = 'user=judymou&url=' + encodeURIComponent(tab.url) +
                    '&selector=' + encodeURIComponent(response.selector);
       xhr.open('GET', 'http://127.0.0.1:3000/watch?' + params, true);
       xhr.send();
-    });
-};
+    });  // chrome.tabs.sendRequest
+}
--- chrome/popup.js
+++ chrome/popup.js
@@ -1,11 +1,12 @@
 chrome.extension.onMessage.addListener(
   function(msg, sender, sendResponse) {
+    if (!msg.popup) return;
     window.alert("Updating popup.html....");
     if (msg.action == 'save') {
       if (msg.success) {
         document.getElementById("status").innerHTML = "Saved " + msg.url;
       } else {
-        document.getElementById("status").innerHTML 
+        document.getElementById("status").innerHTML
           = "Unable to save " + msg.url;
       }
     }
